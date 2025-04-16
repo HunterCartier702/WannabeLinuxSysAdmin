328d30ac75b31ba595015bdcb95daab20390d9c0
Hunter Cartier <huntercartier2@hotmail.com>
2025-04-15 22:22:02 -0700

    Add files via upload

--- tarball.sh
+++ tarball.sh
@@ -2,18 +2,18 @@
 
 # Mostly useless script, but good practice
 
-echo "*Archive and compress*"
-echo "Enter compression type to use: "
+echo "****Archive and Compress***"
+echo -e "Enter compression type to use: \n"
 echo "1 for .gzip"
 echo "2 for .bzip2"
 echo "3 for .xz"
-echo "q to exit"
+echo -e "q to exit \n"
 
 read -p "Enter option: " comp
 
-if [[ "$comp" == "q" ]]; then
+if [[ "$comp" == "q" ]];then
 	exit 0
-elif [[ "$comp" =~ ^[1-3]$ ]]; then
+elif [[ "$comp" =~ ^[1-3]$ ]];then
 	:
 else
 	echo "Invalid input"
@@ -24,31 +24,38 @@ read -p "Enter directory or file to be archived and compressed: " zipped
 
 # Check if the file or directory exists
 if [[ ! -e "$zipped" ]]; then
-    echo "The specified file or directory does not exist."
+    echo "The specified file or directory does not exist. Exiting.."
     exit 1
+elif [[ ! -r "$zipped" && ! -x "$zipped" ]];then  # Check user permissions on file
+	echo "You don't have perms over file. Try running as root. Exiting.."
+	exit 1
+else
+	echo -e "File \""$zipped"\" exists and \"$USER\" has correct perms to archive and compress file. Continuing...\n"
 fi
 
 # Get the base name of the file/folder (without path)
 base="$(basename "$zipped")"
 
 # Ask for output directory (optional)
-read -p "Enter output directory (e.g., /home/user/backups Press 'Enter' for current directory and skip.): " outdir
+read -p "Enter output directory (e.g., /home/user/backups Press 'Enter' for current directory.): " outdir
 outdir="${outdir:-.}"  # Use current directory if none provided
+
+echo -e "\n#########################################\n"
 
 case $comp in
 	1)
 		output="$outdir/$(date +%Y-%m-%d)_$base.tar.gz"
-		echo "Creating archive: $output"
+		echo "Archive $output created from:"
 		tar -czvf "$output" "$zipped"
 		;;
 	2)
 		output="$outdir/$(date +%Y-%m-%d)_$base.tar.bz2"
-		echo "Creating archive: $output"
+		echo "Archive $output created from:"
 		tar -cjvf "$output" "$zipped"
 		;;
 	3)
 		output="$outdir/$(date +%Y-%m-%d)_$base.tar.xz"
-		echo "Creating archive: $output"
+		echo "Archive $output created from:"
 		tar -cJvf "$output" "$zipped"
 		;;
 esac
