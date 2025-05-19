d289dc3bd75760be9e2ac02f84526246e65d28f9
Hunter Cartier <huntercartier2@hotmail.com>
2025-05-18 23:03:33 -0700

    Update mangrep.sh

--- mangrep.sh
+++ mangrep.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# I aded this to my $PATH so I can call it from anywhere
+# I added this to my $PATH so I can call it from anywhere
 # I personally think it is useful if you are curious about a flag from a man page and just want to check it fast.
 # I would type this command out all the time 
-# If you're always doing something more than once than why not automate it no matter how simple
+# If you're always doing something more than once then why not automate it no matter how simple
 
 # Example: man tar | grep -C2 "\-C"
 
