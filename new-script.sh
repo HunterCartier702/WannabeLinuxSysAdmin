8b4d00e0eaa580a57ccc705f0a2054e629025d2c
Hunter Cartier <huntercartier2@hotmail.com>
2026-04-12 01:32:23 -0700

    Add files via upload

---
+++ new-script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Create 1 or more blank bash script templates
+# Add to /usr/local/bin if it is in your $PATH for quick access
+
+function usage
+{
+	# Strip off the full path and "pretty print" only the script name itself
+	script=${0##*/}
+	echo "Usage: $script $@"
+	exit 1
+}
+
+if [[ $# -lt 1 || "$1" == "-h" || "$1" == "--help" ]];then
+	usage "<script1>" "[arg2...]"
+fi
+
+for arg in "$@";do
+	if [ -a "$arg" ];then
+		echo -e "\n$arg already exists, skipping..\n"
+		continue
+	else
+		echo -e '#!/bin/bash\n' > "$arg"
+		chmod u+x "$arg"
+		echo "------------------"
+		echo -e "Created $arg\nContents:"
+		cat "$arg"
+		echo "------------------"
+	fi
+done
