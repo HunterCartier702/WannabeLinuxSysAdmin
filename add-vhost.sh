8a1454a4c99fe49b675d96f7bdc1a8d36d26f929
Hunter Cartier <huntercartier2@hotmail.com>
2026-04-20 13:11:24 -0700

    Delete add-vhost.sh

--- add-vhost.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-read -p "Enter IP: " IP
-read -p "Enter or copy/paste vhosts list: " vhosts
-cat /etc/hosts
-echo "------"
-printf "\n%s\t%s\n" "$IP" "$vhosts" | sudo tee -a /etc/hosts
-echo "------"
-cat /etc/hosts
